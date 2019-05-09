ea87e20b65241b2de8e7dfd90bcd803b6a5ecac6
RyanKilbane <RyanKilbane92@hotmail.com>
2019-05-09 15:08:59 +0100

    Updated Take-On-IG-SG to be able to use 'ALL' protocol

--- aws-terraform/vpc.tf
+++ aws-terraform/vpc.tf
@@ -45,8 +45,8 @@ resource "aws_security_group" "Take-On-IG-SG" {
   description = "TakeOn security group for our internet gateway"
   vpc_id = "${aws_vpc.Take-On-VPC.id}"
   ingress {
-      from_port = 80
-      to_port = 80
+      from_port = 0
+      to_port = 0
       protocol = "-1"
       cidr_blocks = ["0.0.0.0/0"]
   }
