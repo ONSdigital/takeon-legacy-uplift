f1c6716e15e239e01339ca0e129e3aaaf4d9dda0
RyanKilbane <RyanKilbane92@hotmail.com>
2019-05-16 11:20:18 +0100

    corrected subnet id

--- aws-terraform/ec2.tf
+++ aws-terraform/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "Test-Instance" {
   ami           = "${var.amis["take-on-test"]}"
   instance_type = "t2.micro"
   associate_public_ip_address = false
-  subnet_id = "${var.takeon_private_subnet.id}"
+  subnet_id = "${aws_subnet.private-subnet.id}"
   key_name = "instance-test"
   tags {
       Team = "TakeOn"
