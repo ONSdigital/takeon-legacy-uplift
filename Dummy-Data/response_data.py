d8375dcf249a54c64d393aa21142c7bfda63468d
Jamie Howells <jim-74@live.com>
2019-05-28 13:51:01 +0100

    Code ready for review

--- Dummy-Data/response_data.py
+++ Dummy-Data/response_data.py
@@ -1,3 +1,15 @@
+"""
+Generates a dummy file based on QCAS survey input data supplied by the electronic questionnaire
+The output file created is in JSON format
+Requires a dummy finalsel file as input (created in the 'generate dummy' script
+
+Input Parameters -
+selection file location: 'finalsel file, with csv extention'
+response file location: 'Name of the output file'
+survey code: 'eg. 019'
+Period: 'eg. 202003'
+"""
+
 import json
 import random
 from faker import Faker
@@ -8,6 +20,10 @@ output_location = input("response file location: ")
 survey = input("survey code: ")
 period = input("period: ")
 
+month = period[4:]
+year = period[:4]
+#response_list = ["Yes", "No"]
+
 
 def create_file_paths(selection_location, output_location):
     return {"selection": selection_location, "response": output_location}
@@ -16,6 +32,7 @@ def create_file_paths(selection_location, output_location):
 class ResponseData:
     def __init__(self, survey, selection, response_file, period):
         file_dict = create_file_paths(selection, response_file)
+        self.response_list = ["Yes", "No"]
         self.survey = survey
         self.file = file_dict["selection"]
         self.q_codes1 = {'019': [678, 679]}
@@ -26,14 +43,6 @@ class ResponseData:
         self.data = []
         self.output = file_dict["response"]
         self.period = period
-
-        self.data1 = {i: str(random.randint(1, 2)) for i in self.q_codes1[self.survey]}
-        data2 = {i: fake.date(pattern='%Y-%m-%d') for i in self.q_codes2[self.survey]}
-        data3 = {i: str(random.randrange(0, 9999999999)) for i in self.q_codes3[self.survey]}
-        data4 = {i: fake.paragraph() for i in self.q_codes4[self.survey]}
-        self.data1.update(data2)
-        self.data1.update(data3)
-        self.data1.update(data4)
 
     def get_selection_list(self):
         with open(self.file, 'r') as selection_file:
@@ -46,7 +55,17 @@ class ResponseData:
             self.refs.append(row.split(':')[0])
 
     def generate_response_data(self):
+#        self.resp_list = self.response_list
         for ref in self.refs:
+            self.data1 = {i: str(random.randint(1, 2)) for i in self.q_codes1[self.survey]}
+            data2 = {i: fake.date(pattern='%Y-%m-%d') for i in self.q_codes2[self.survey]}
+            data3 = {i: str(random.randrange(0, 9999999999)) for i in self.q_codes3[self.survey]}
+            data4 = {i: random.choice(self.response_list) for i in self.q_codes4[self.survey]}
+            self.data1.update(data2)
+            self.data1.update(data3)
+            self.data1.update(data4)
+
+
             datum = {
                 "submission": {
                     "case_id": fake.uuid4(),
@@ -58,8 +77,8 @@ class ResponseData:
                     "data": self.data1,
                     "flushed": str(fake.boolean()),
                     "metadata": {
-                        "ref_period_end_date": fake.date(pattern='%d-%m-%Y'),
-                        "ref_period_start_date": fake.date(pattern='%d-%m-%Y'),
+                        "ref_period_end_date": str(year + '-' + month + '-' + '25'),
+                        "ref_period_start_date": str(year + '-' + month + '-' + '01'),
                         "ru_ref": ref,
                         "user_id": fake.user_name()
                     },
